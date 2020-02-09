3f341db93f8e50ffb8aaa316539857b49fa14e59
Maria Connie Sanchez <msnchz04@gmail.com>
2020-02-09 03:43:05 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,4 +1,5 @@
 class Shoe
+  attr_accessor :brand, :color
   def initialize(brand)
     @brand = brand 
   end
@@ -8,7 +9,7 @@ class Shoe
   end 
   
   def color
-    puts "red"
+    @color 
   end 
   
   
